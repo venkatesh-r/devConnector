29a2310ce43b76342d19e1026a3de49b32c27c78
venkatesh-r <venkypsd@gmail.com>
2024-10-31 17:25:31 +0100

    validation added

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,9 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
